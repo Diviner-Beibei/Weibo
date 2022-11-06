27126c9cc66c72c43976e7cc82ee3f0767f48047
Beibei <rookiepbb@gmail.com>
2022-11-06 20:53:07 +0000

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,6 @@ Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name(
 Route::post('password/reset',  'PasswordController@reset')->name('password.update');
 //微博相关操作
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+//关注人列表和粉丝列表
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
